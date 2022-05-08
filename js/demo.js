e05b3de2358e03ca09abd40d7e7bace9702e3423
Paul Amazona <whatevergeek@gmail.com>
2022-05-08 16:33:08 +0000

    added initial files

---
+++ js/demo.js
@@ -0,0 +1,13 @@
+function makeMusic() {
+    document.getElementById("output").innerText = "Processing..."
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now()
+    synth.triggerAttack("D4", now);
+    synth.triggerAttack("F4", now + 0.5);
+    synth.triggerAttack("A4", now + 1);
+    synth.triggerAttack("C5", now + 1.5);
+    synth.triggerAttack("E5", now + 2);
+    synth.triggerRelease(["D4", "F4", "A4", "C5", "E5"], now + 4);
+
+    document.getElementById("output").innerText = "Processing Done."
+}
